87afaf792d4a813736b691c42038ed4c5f6ff0d4
Pritam0044 <you@example.com>
2022-04-07 23:33:04 +0530

    apr7

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,15 +3,23 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/all-candidates', function (req, res) {
+    res.send('My first api!')
+    let batch =["Bharat", "Akash","Sabiha","Suyash","Pritesh","Ram","Ramratan","Niladri","Pritam","Nilesh"]
+    console.log(batch);
 });
 
-
+router.get('/candidates', function (req, res) {
+    let batch =["Bharat", "Akash","Sabiha","Suyash","Pritesh","Ram","Ramratan","Niladri","Pritam","Nilesh"]
+    
+    if(req.query.count>0 && req.query.count<=10){
+        for(let i=0; i<req.query.count; i++){
+            console.log(batch[i]);
+        }
+    } else {console.log("The count number is exceeded.")};
+   
+    res.send('My first api!')
+});
 
 
 module.exports = router;
