6c109563c970d7f1392b30fe9621104b68a0e238
Pritam0044 <you@example.com>
2022-04-12 14:02:37 +0530

    April 12 2:03pm

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,17 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
+const bookSchema = new mongoose.Schema( {
+    bookName: String,
+    authorName: String,
+    isbn: {
+        type: Number,
         unique: true,
         required: true
     },
-    emailId: String,
-    gender: {
+    category: {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+        enum: ["novel", "comic", "fairytale","study", "adventure", "drama", "poem","general"] 
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
-
-
-
-// String, Number
-// Boolean, Object/json, array
+module.exports = mongoose.model('User', bookSchema) 
